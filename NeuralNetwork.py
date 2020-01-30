51660873fca17ec989c705760ba36e3ec6dbb2c7
EvanEzell <evanezell595@gmail.com>
2020-01-30 01:25:45 -0500

    initial commit

---
+++ NeuralNetwork.py
@@ -0,0 +1,165 @@
+import numpy, sys 
+
+class Neuron:
+    
+    def __init__(self, activation, num_inputs, eta, weights):
+        self.activation = activation
+        self.num_inputs = num_inputs
+        self.eta = eta
+        self.weights = []
+        if weights == "random":
+            for i in range(num_inputs+1):
+               self.weights.append(numpy.random.random())
+        else:
+            self.weights = weights
+
+    def activate(self, x):
+        return self.activation(x)
+
+    def calculate(self, data):
+        total = 0;
+        for i in range(len(data)):
+            total += data[i] * self.weights[i]
+
+        return self.activate(total + self.weights[len(data)])
+
+    def print_neuron(self):
+        print("num_inputs: " + str(self.num_inputs))
+        print("eta: " + str(self.eta))
+        count = 0
+        for i in self.weights:
+            print("weight[" + str(count) + "]: " + str(i))
+            count += 1
+
+class FullyConnectedLayer:
+    def __init__(self, num_neurons, activation, num_inputs, eta, weights):
+        self.num_neurons = num_neurons
+        self.activation = activation
+        self.num_inputs = num_inputs
+        self.eta = eta
+
+        self.neurons = []
+        if weights == "random":
+            for i in range(self.num_neurons):
+                self.neurons.append(Neuron(self.activation, self.num_inputs, self.eta, "random"))
+        else:
+            for i in range(self.num_neurons):
+                self.neurons.append(Neuron(self.activation, self.num_inputs, self.eta, weights[i]))
+
+    def calculate(self, data):
+        output = []
+        for i in range(self.num_neurons):
+            output.append(self.neurons[i].calculate(data))
+
+        print(output)
+        return output
+
+    def print_layer(self):
+        for i in range(self.num_neurons):
+            print("neuron " + str(i))
+            self.neurons[i].print_neuron()
+            print("")
+
+class NeuralNetwork:
+    def __init__(self, num_layers, num_neurons, activation, num_inputs, loss, eta, weights):
+        self.num_layers = num_layers
+        self.num_neurons = num_neurons
+        self.activation = activation
+        self.num_inputs = num_inputs
+        self.loss = loss
+        self.eta = eta
+
+        self.layers = []
+        prev_inputs = self.num_inputs
+        if weights == "random":
+            for i in range(self.num_layers):
+                self.layers.append(FullyConnectedLayer(self.num_neurons[i], self.activation[i], prev_inputs, self.eta, "random"))
+                prev_inputs = self.num_neurons[i]
+        else:
+            for i in range(self.num_layers):
+                self.layers.append(FullyConnectedLayer(self.num_neurons[i], self.activation[i], prev_inputs, self.eta, weights[i]))
+                prev_inputs = self.num_neurons[i]
+
+    def calculate(self, data):
+        output = data
+        for layer in self.layers:
+            output = layer.calculate(output)
+        return output
+
+    def calculateloss(self, prediction, target):
+        return self.loss(prediction, target)
+
+    def train(self, data, truth):
+        output = self.calculate(data)
+        print("training...")
+        print(output)
+        self.layers[-1].print_layer()
+
+        for neuron in self.layers[-1].neurons:
+            print("weights before ")
+            print(neuron.weights)
+            for i, weight in enumerate(neuron.weights):
+                derivative = (neuron.activate(weight + 0.01) - neuron.activate(weight)) / 0.01
+                neuron.weights[i] = weight - neuron.eta * derivative
+            print("weights after ")
+            print(neuron.weights)
+
+        #updated_weight = old_weight - self.eta * derivative
+
+#        self.layers[-1].neurons[0].print_neuron()
+#        weight = self.layers[-1].neurons[0].weights[0]
+#        print("weight: " + str(weight))
+#        derivative = (self.layers[-1].neurons[0].activate(weight + 0.01) - self.layers[-1].neurons[0].activate(weight)) / 0.01
+#        print("derivative: " + str(derivative))
+#        new_weight = weight - self.layers[-1].neurons[0].eta * derivative
+#        print("new weight: " + str(new_weight))
+        
+
+    def print_nn(self):
+        for i in range(self.num_layers):
+            print("Layer " + str(i))
+            print("Number of Neurons in Layer: " + str(self.layers[i].num_neurons))
+            self.layers[i].print_layer()
+
+def main():
+    choices = ['example', 'and', 'xor']
+    if len(sys.argv) != 2 or sys.argv[1] not in choices:
+        print("Please provide one of the following command line arguments:")
+        print("'example', 'and', or 'xor'")
+        exit()
+
+    print("Choice: " + sys.argv[1])
+
+    if sys.argv[1] == 'example':
+        print("running example")
+        num_layers = 2
+        num_neurons = [2,2]
+        weights = "random"
+        weights = [[[.15,.20,.35],[.25,.30,.35]],[[.40,.45,.60],[.50,.55,.60]]]
+
+        activation = []
+        for i in range(num_layers):
+            activation.append(lambda x : 1 / (1 + numpy.exp(-x)))
+
+        num_inputs = 2
+
+        def loss(prediction, target):
+            total = 0
+            for i in range(len(prediction)):
+                total += .5*((prediction[i]-target[i])**2)
+            return total
+
+        nn = NeuralNetwork(num_layers, num_neurons, activation, num_inputs, loss, .5, weights)
+        nn.print_nn()
+
+        print(nn.calculate([.05,.10]))
+        print(nn.calculateloss(nn.calculate([.05,.10]),[.01,.99]))
+
+    elif sys.argv[1] == 'and':
+        print("running and")
+    else:
+        print("running xor")
+
+if __name__ == '__main__':
+    main()
+
